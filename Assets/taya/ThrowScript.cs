cd1e0242f4ffdce68df87ecbb280ed49b4b59a47
YusukeTamura <yusuke.tamura@crowdworks.co.jp>
2018-12-08 12:51:10 +0900

    手の動きのタイミングをスクリプトで制御できるように変更（発火タイミング調整中）

--- Assets/taya/ThrowScript.cs
+++ Assets/taya/ThrowScript.cs
@@ -8,7 +8,11 @@ public class ThrowScript : MonoBehaviour
     public Camera camera;
     public static bool isDisplayed = true;
     public static bool isResult;
+
     public ParticleSystem hitParticlePrefab; // 的に当たったときのパーティクル
+
+    public GameObject hand;
+    public Animator throwHand;
 
     private AudioSource audioSource;        // AudioSorceを格納する変数の宣言.
     public AudioClip sound;             // 効果音を格納する変数の宣言.
@@ -21,10 +25,12 @@ public class ThrowScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        throwHand = hand.GetComponent<Animator>();
+        throwHand.SetBool("Play", false);
+
         audioSource = gameObject.AddComponent<AudioSource>();   // AudioSorceコンポーネントを追加し、変数に代入.
         audioSource.clip = sound;       // 鳴らす音(変数)を格納.
         audioSource.loop = false;       // 音のループなし。
-
         transform.position = RayScript.handPosition;
         r = GetComponent<Rigidbody>();
 
@@ -71,6 +77,9 @@ public class ThrowScript : MonoBehaviour
 
             // 矢が当たったとき、音を鳴らす
             audioSource.Play();
+
+            // 手の動きを再生
+            throwHand.SetBool("Play", true);
 
             //Debug.Log(collider.gameObject.name);
             GetComponent<Rigidbody>().velocity = Vector3.zero;
